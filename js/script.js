bb65f5c200c4b2c44aa85beaa9afb4659f5f797d
GiovanniConti <giovanni.conti@live.it>
2021-10-13 09:50:10 +0200

    changed if iteration

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
 const containerBox = document.querySelector(".container-box");
 
-for(let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     const box = document.createElement("div");
     box.classList.add("box", "ratio", "ratio-1x1");
 
-    if (i % 3 === 0){
-
-        if(i % 5 === 0){
-            box.classList.add("multiple-3-5")
-            box.innerHTML += `<h2>fizzbuzz</h2>`
-        } else{
-            box.classList.add("multiple-3");
-            box.innerHTML += `<h2>fizz</h2>`
-        }
-    } else if (i % 5 === 0){
+    if (i % 3 === 0 && i % 5 === 0) {
+        box.classList.add("multiple-3-5")
+        box.innerHTML += `<h2>fizzbuzz</h2>`
+    } else if (i % 3 === 0) {
+        box.classList.add("multiple-3");
+        box.innerHTML += `<h2>fizz</h2>`
+    } else if (i % 5 === 0) {
         box.classList.add("multiple-5")
         box.innerHTML += `<h2>buzz</h2>`
-    } else{
+    } else {
         box.innerHTML += `<h2>${i}</h2>`
     }
-    
+
     containerBox.append(box)
 }
